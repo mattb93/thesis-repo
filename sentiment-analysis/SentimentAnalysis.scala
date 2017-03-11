35db2b6e37b898aeebe786d5e8d7af219e09cf26
mattb93 <mattb93@vt.edu>
2017-03-11 16:39:12 -0500

    First succesful sentiment analysis run. Needs refinement.

--- sentiment-analysis/SentimentAnalysis.scala
+++ sentiment-analysis/SentimentAnalysis.scala
@@ -2,45 +2,37 @@
 // 2. Farm those tweets for commons words and/or frequent patterns
 // 3. Expand collection of seed words by running frequent pattern mining on tweets that contain those frequent patterns
 
-import edu.vt.dlib.api._
+import edu.vt.dlib.api.io._
+
+import org.apache.spark.rdd.RDD
 
 // Configutation parameters
 val initialNumSeedWords = 5 // initial number of words to be extracted from self-labeled tweets
 val targetNumSeedWords = 10	// number of seed words to pull from all tweets
 val numIterations = 10		// number of times to repeat searching for new seed words
-val orientationSeed = """:\(""".r // regular epression use to start search
+val orientationSeed = ":(" // regular expression use to start search
 
-val collectionNumber = 41
-val colelctionID = "NewtownShooting"
+val collectionNumber = "41"
+val collectionID = "NewtownShooting"
 
 // Get the collection and clean it
 val collection: TweetCollection = new HDFSTweetCollection(collectionID, sc, sqlContext, collectionNumber)
 val cleanCollection: TweetCollection = collection.removeStopWords().removeRTs().removeMentions().removeURLs().removePunctuation().toLowerCase()
 
 // Find the initial set of self-labeled sentiment oriented tweets
-val featureExtractor = new featureExtractor(cleanCollection)
-val orientedTweets: RDD[String] = cleanCollection.extractRegexMatches(orientationSeed).map(entry => entry._1)
+var orientedTweets: RDD[String] = collection.getTextArrays().filter(tokens => tokens.contains(orientationSeed)).map(tokens => tokens.mkString(" "))
 
 // Get the top initialNumSeedWords most frequent words. This will be oriented in whichever direction is desired by orientationSeed
-val orientedTokens: Array[String] = orientedTweets.flatMap(text => text.split(" "))
-                    .map(word => (word, 1))
-                    .reduceByKey(_ + _)
-                    .sortBy(_._2, false)
-                    .take(initialNumSeedWords)
-                    .collect();
+var orientedTokens: Array[(String, Int)] = orientedTweets.flatMap(text => text.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._2, false).take(initialNumSeedWords)
 
 // Run the process over a few times to get a solid representation
-for( var i <- 0 to numIterations) {
+var i = 0
+for(i <- 0 to numIterations) {
 	// Filter for tweets that contain at least one of the already defined seed words
-	orientedTweets = collection.getTextArrays().filter(tokens => ! tokens.union(orientedTokens).isEmpty()).map(tokens => tokens.mkString(" "))
+	orientedTweets = collection.getTextArrays().filter(tokens => ! tokens.union(orientedTokens).isEmpty).map(tokens => tokens.mkString(" "))
 
 	// Search through the tweets for the top targetNumSeedWords most frequent words
-	orientedTokens = orientedTweets.flatMap(text => text.split(" "))
-                .map(word => (word, 1))
-                .reduceByKey(_ + _)
-                .sortBy(_._2, false)
-                .take(targetNumSeedWords)
-                .collect();
+	orientedTokens = orientedTweets.flatMap(text => text.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._2, false).take(targetNumSeedWords)
 }
 
 orientedTokens.foreach(println)
