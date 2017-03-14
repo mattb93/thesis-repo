7a88af1a1e160fce2b2b3aa8486348cca4bf3800
mattb93 <mattb93@vt.edu>
2017-03-14 17:29:44 -0400

    First successful run of word2vec sentiment analysis

--- sentiment-analysis/Word2VecSentimentAnalysis.scala
+++ sentiment-analysis/Word2VecSentimentAnalysis.scala
@@ -2,29 +2,32 @@
 // 2. Farm those tweets for commons words and/or frequent patterns
 // 3. Expand collection of seed words by running frequent pattern mining on tweets that contain those frequent patterns
 
-import edu.vt.dlib.api.io._
+import edu.vt.dlib.api.io.{TweetCollection, HDFSTweetCollection}
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 import org.apache.spark.mllib.regression.LabeledPoint
-
+import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, LogisticRegressionModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.DenseVector
 
 import java.io._
 
-class Word2VecSentimentAnalysis() {
+class Word2VecSentimentAnalysis() extends Serializable {
     
 
     // Configutation parameters
     val tokensToDiscard = Array("connecticut", "shooting", "school", "sandy", "hook")
     val collectionNumber = "41"
     val collectionID = "NewtownShooting"
-    val oritntationSeed = ":("
+    val orientationSeed = ":("
 
     val word2VecModelLength = 70
-    val word2VecModel : Word2VecModel = null
+    val tweetLength = 70
+    var word2VecModel : Word2VecModel = null
 
     val numClasses = 2
-    val classifierModel : LogisticRegressionModel = null
+    var classifierModel : LogisticRegressionModel = null
 
     def run() = {
         // Get the collection and clean it
@@ -32,38 +35,43 @@ class Word2VecSentimentAnalysis() {
         val cleanCollection: TweetCollection = collection.removeStopWords().removeRTs().removeMentions().removeURLs().toLowerCase().removeTokens(tokensToDiscard)
 
         // Format corpus as RDD of Seq for Word2Vec. This is only tweets which contained the oriented emoji we're looking for.
-        val orientedTweets: RDD[(String, Array[String])] = cleanCollection.getTextArraysID().filter(entry => entry._2.contains(orientationSeed))
+        val orientedTweets: RDD[(Double, Array[String])] = cleanCollection.getTextArrays().filter(entry => entry.contains(orientationSeed)).map(entry => (0, entry))
 
         // Fit a word2Vec model with the oriented data
         val word2Vec = new Word2Vec()
-        word2Vec.setModelLength(word2VecModelLength)
+        word2Vec.setVectorSize(word2VecModelLength)
         word2VecModel = word2Vec.fit(orientedTweets.map(entry => entry._2.toSeq))
 
         // Need an RDD of LabeledPoints to pass to LogisticRegressionWithLBFGS
         // Get the features
         val training = orientedTweets.map(tweet => createLabeledPoint(tweet)).cache()
-        val data = cleanCollection.map(tweet => createLabeledPoint(tweet)).cache()
+        val data = cleanCollection.getTextArrays().map(tweet => (tweet.mkString(" "), createFeatureVector(tweet))).cache()
 
         classifierModel = new LogisticRegressionWithLBFGS().setNumClasses(numClasses).run(training)
 
-        val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+        val predictions = data.map { case (text, features) =>
             val prediction = classifierModel.predict(features)
-            (prediction, label)
+            (text, prediction)
         }
 
-        predictionAndLabels.take(100).foreach(println)
+        predictions.take(100).foreach(println)
     }
 
-    def createLabeledPoint(tweet: (String, Array[String])): LabeledPoint = {
-        val features = tweet._2.map(word => getAveragedWordVector(word))
-        features = Vectors.dense(PadFeatureArray(features))
+    def createFeatureVector(tweet: Array[String]): org.apache.spark.mllib.linalg.Vector = {
+        val doubles: Array[Double] = tweet.map(word => getAveragedWordVector(word))
+        return Vectors.dense(PadFeatureArray(doubles))
+    }
 
-        val labeledPoint = new LabeledPoint(tweet._1.toDouble, features)
+    def createLabeledPoint(tweet: (Double , Array[String])): LabeledPoint = {
+        val features: Array[Double] = tweet._2.map(word => getAveragedWordVector(word))
+        val featuresVector = Vectors.dense(PadFeatureArray(features))
+
+        val labeledPoint = new LabeledPoint(tweet._1, featuresVector)
 
         return labeledPoint
     }
 
-    def getAveragedWordVector(word: String) = {
+    def getAveragedWordVector(word: String): Double = {
         var sum = 0.0
         try {
           val transformedVector = word2VecModel.transform(word)
@@ -72,7 +80,7 @@ class Word2VecSentimentAnalysis() {
           case _ => sum = 0.0
         }
 
-        return sum/_word2VecModelLength
+        return sum/word2VecModelLength
     }
 
     def PadFeatureArray(features: Array[Double]): Array[Double] = {
@@ -85,4 +93,5 @@ class Word2VecSentimentAnalysis() {
 }
 
 
-
+val w2va = new Word2VecSentimentAnalysis()
+w2va.run()
